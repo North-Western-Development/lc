ea5ae89636bc70924f6dd4bdcef04d16fbcb059e
Jackson Abney <hickorysmokedbacon.yt@gmail.com>
2024-05-25 19:46:23 -0800

    Work in progress projector fixes

--- src/main/java/li/cil/oc2/common/item/CPUItem.java
+++ src/main/java/li/cil/oc2/common/item/CPUItem.java
@@ -1,5 +1,9 @@
 package li.cil.oc2.common.item;
 
+import li.cil.oc2.common.util.TextFormatUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+
 public class CPUItem extends ModItem {
 
     private final int frequency;
@@ -12,5 +16,24 @@ public class CPUItem extends ModItem {
     public int getFrequency()
     {
         return frequency;
+    }
+
+    public String getFrequencyString()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(frequency/1_000_000);
+        builder.append(" MHz");
+
+        return builder.toString();
+    }
+
+    @Override
+    public Component getName(final ItemStack stack) {
+        return Component.literal("")
+            .append(super.getName(stack))
+            .append(" (")
+            .append(getFrequencyString())
+            .append(")");
     }
 }
