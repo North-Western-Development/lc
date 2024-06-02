5e4598bbe666266ce10caf3fc843db4514c8c8fd
Jackson Abney <hickorysmokedbacon.yt@gmail.com>
2024-06-02 00:16:27 -0800

    Documentation updates and fixes for both the double break issue and the monitor not using power or having it's own config value for power. Also sets the VXLAN block config option to disabled by default. Though this block is hidden as I don't like its current implementation, and I'm still working on a better alternative.

--- src/main/java/li/cil/oc2/common/blockentity/ModBlockEntity.java
+++ src/main/java/li/cil/oc2/common/blockentity/ModBlockEntity.java
@@ -16,7 +16,6 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public abstract class ModBlockEntity extends BlockEntity {
     private final Runnable onWorldUnloaded = this::onWorldUnloaded;
